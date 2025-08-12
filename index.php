3648350a7f0556cc82d5de9dac9b837ccb1ae971
WHOISYXM? <damnforwhat22@icloud.com>
2025-08-12 19:33:20 +0500

    add search form / css update

--- index.php
+++ index.php
@@ -10,6 +10,22 @@
     <link href="assets/styles.css" rel="stylesheet">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <!-- Поисковая форма -->
+    <form class="d-flex align-items-center search-form">
+      <div class="input-group search-container">
+        <input type="search" class="form-control search-input" placeholder="Поиск...">
+        <button class="btn search-btn" type="submit">
+          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+            <circle cx="11" cy="11" r="8"></circle>
+            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+          </svg>
+        </button>
+      </div>
+    </form>
+  </div>
+</nav>
     <div class="container-fluid h-100">
         <div class="row h-100">
             <!-- Left container - post form -->
@@ -26,7 +42,7 @@
             <div class="mb-3">
                 <textarea class="form-control" required id="postContent" rows="10" placeholder="Напишите ваш текст здесь..."></textarea>
             </div>
-            <button type="submit" class="btn btn-dark">Опубликовать</button>
+            <button type="submit" class="btn">Опубликовать</button>
         </form>
     </div>
 
